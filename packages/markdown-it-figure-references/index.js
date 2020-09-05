9f78523ae4535caeab1d1aa204bd798db2583ca3
Alija Sabic <sabic.alija@gmail.com>
2020-09-05 17:31:57 +0200

    markdown-it-figure-references: reload rule with new option if already existing, refactor custom image rendering/wrapping

--- packages/markdown-it-figure-references/index.js
+++ packages/markdown-it-figure-references/index.js
@@ -1,21 +1,28 @@
 const figure_references = (md, opts) => {
   opts = Object.assign({}, figure_references.defaults, opts);
 
-  md.core.ruler.push("figure_reference", figure_reference_rule(opts));
-  md.core.ruler.push("figure_reference_list", figure_reference_list_rule(opts));
+  const reload = md.core.ruler.getRules("").find(({ name }) => name === "figure_reference");
+  if (reload) {
+    md.core.ruler.at("figure_reference", figure_reference_rule(opts));
+    md.core.ruler.at("figure_reference_list", figure_reference_list_rule(opts));
+  } else {
+    md.core.ruler.push("figure_reference", figure_reference_rule(opts));
+    md.core.ruler.push("figure_reference_list", figure_reference_list_rule(opts));
+  }
 
   md.renderer.rules.figure_reference_list_open = figure_reference_list_open_renderer(opts);
   md.renderer.rules.figure_reference_list_item = figure_reference_list_item_renderer(opts);
   md.renderer.rules.figure_reference_list_close = figure_reference_list_close_renderer(opts);
 
   if (opts.wrapImage) {
-    const defaultImageRenderer = md.renderer.rules.image;
-    md.renderer.rules.image = figure_wrapper_renderer(opts, defaultImageRenderer);
+    md.renderer.rules.figure_wrapper = figure_wrapper_renderer(opts, md.renderer.rules.image);
+  } else if (reload) {
+    md.renderer.rules.figure_wrapper = md.renderer.rules.image;
   }
 };
 
 function figure_reference_rule(opts) {
-  return (state /* , silent */) => {
+  const figure_reference = (state /* , silent */) => {
     const tokens = state.tokens;
 
     for (let i = 0; i < tokens.length; i++) {
@@ -29,6 +36,8 @@ function figure_reference_rule(opts) {
 
         const title = token.attrGet("title") || token.attrGet("alt");
         if (!title) continue;
+
+        token.type = "figure_wrapper";
 
         if (!state.env.figures) {
           state.env.figures = {};
@@ -60,10 +69,11 @@ function figure_reference_rule(opts) {
       }
     }
   };
+  return figure_reference;
 }
 
 function figure_reference_list_rule(opts) {
-  return (state) => {
+  const figure_reference_list = (state) => {
     if (!state.env.figures || !opts.list) return;
     const tokens = state.tokens;
     const figures = [];
@@ -71,7 +81,7 @@ function figure_reference_list_rule(opts) {
       .filter((tok) => tok.type === "inline")
       .filter((tok) => {
         for (const child of tok.children) {
-          if (child.type === "image") {
+          if (child.type === "figure_wrapper") {
             figures.push(child);
           }
         }
@@ -93,6 +103,7 @@ function figure_reference_list_rule(opts) {
     token.block = true;
     tokens.push(token);
   };
+  return figure_reference_list;
 }
 
 function figure_reference_list_open_renderer(opts) {
