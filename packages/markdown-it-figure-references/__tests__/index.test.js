6c4dc013d75d5f9b1a0ab2ebc0838444838346ae
Alija Sabic <sabic.alija@gmail.com>
2020-09-05 17:13:35 +0200

    markdown-it-figure-references: create test suite and test case generation script

--- packages/markdown-it-figure-references/__tests__/index.test.js
+++ packages/markdown-it-figure-references/__tests__/index.test.js
@@ -1,40 +1,90 @@
-const text = `# Hello World
-
-![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
-
-![Minion](https://octodex.github.com/images/minion.png "minion#The Minion")
-`;
+const { join } = require("path");
+const { readFileSync } = require("fs");
 
 const MarkdownIt = require("markdown-it");
 const MarkdownItPluginFigureReferences = require("./../index.js");
 const md = new MarkdownIt({ xhtmlOut: true, html: true });
 
-md.use(MarkdownItPluginFigureReferences);
+describe("markdown-it handling", function () {
+  it("reload rule", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences);
+    md.use(MarkdownItPluginFigureReferences);
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "auto-id.html"), { encoding: "utf8" }));
+  });
+});
 
-test("figure reference basic renderering test", () => {
-  const result = md.render(text);
-  expect(result).toBe(
-    "<h1>Hello World</h1>\n" +
-      '<p><div class="figure-wrapper" id="the-stormtroopocat">\n' +
-      "  <figure>\n" +
-      '    <img src="https://octodex.github.com/images/stormtroopocat.jpg" alt="Stormtroopocat" title="The Stormtroopocat" id="the-stormtroopocat" />\n' +
-      "    <figcaption>\n" +
-      '      <a href="#the-stormtroopocat">Figure 1</a>: The Stormtroopocat\n' +
-      "    </figcaption>\n" +
-      "  </figure>\n" +
-      "</div></p>\n" +
-      '<p><div class="figure-wrapper" id="minion">\n' +
-      "  <figure>\n" +
-      '    <img src="https://octodex.github.com/images/minion.png" alt="Minion" title="The Minion" id="minion" />\n' +
-      "    <figcaption>\n" +
-      '      <a href="#minion">Figure 2</a>: The Minion\n' +
-      "    </figcaption>\n" +
-      "  </figure>\n" +
-      "</div></p>\n" +
-      '<h2 id="list-of-figures"><a href="#list-of-figures" class="header-anchor">#</a>List of Figures</h2>\n' +
-      '<ol class="list-of-figures-list">\n' +
-      '  <li><a href="#the-stormtroopocat">Figure 1</a>: The Stormtroopocat</li>\n' +
-      '  <li><a href="#minion">Figure 2</a>: The Minion</li>\n' +
-      "</ol>"
-  );
+describe("basic functionality", function () {
+  it("automatic id insertion", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences);
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "auto-id.html"), { encoding: "utf8" }));
+  });
+  it("manual id insertion", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "trooper#The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences);
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "manual-id.html"), { encoding: "utf8" }));
+  });
+  it("multiple images", () => {
+    const text =
+      `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")\n` +
+      `![Minion](https://octodex.github.com/images/minion.png "minion#The Minion")`;
+    md.use(MarkdownItPluginFigureReferences);
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "multiple-images.html"), { encoding: "utf8" }));
+  });
+});
+
+describe("options", function () {
+  it("option: list", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences, { list: false });
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "option-list.html"), { encoding: "utf8" }));
+  });
+
+  it("option: listTitle", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences, { listTitle: "List of Images" });
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "option-list-title.html"), { encoding: "utf8" }));
+  });
+
+  it("option: listTag", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences, { listTag: "ul" });
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "option-list-tag.html"), { encoding: "utf8" }));
+  });
+
+  it("option: label", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences, { label: "Image" });
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "option-label.html"), { encoding: "utf8" }));
+  });
+
+  it("option: wrapImage", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences, { wrapImage: false });
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "option-wrap-image.html"), { encoding: "utf8" }));
+  });
+
+  it("option: wrapTag", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences, { wrapTag: "span" });
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "option-wrap-tag.html"), { encoding: "utf8" }));
+  });
+
+  it("option: injectLabel", () => {
+    const text = `# Hello World\n\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+    md.use(MarkdownItPluginFigureReferences, { injectLabel: false });
+    const result = md.render(text);
+    expect(result).toBe(readFileSync(join(__dirname, "html", "option-inject-label.html"), { encoding: "utf8" }));
+  });
 });
