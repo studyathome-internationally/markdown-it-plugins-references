f859a1487d09ee9d4c404b735f44a72439f64b07
Alija Sabic <sabic.alija@gmail.com>
2020-09-07 03:22:19 +0200

    markdown-it-references: update snapshots with new serializer

--- packages/markdown-it-references/__tests__/markdown.test.js
+++ packages/markdown-it-references/__tests__/markdown.test.js
@@ -11,27 +11,17 @@ md.use(require("markdown-it-figure-references"), {
 
 describe("markdown-it handling", () => {
   it("reload rule", () => {
-    const text = readFileSync(join(__dirname, "__cases__", "basic.figure.1.md"), "utf8");
+    const text = readFileSync(
+      join(__dirname, "__cases__", "basic.figure.1.md"),
+      "utf8"
+    );
     md.use(MarkdownItPluginReferences);
     md.use(MarkdownItPluginReferences);
     const result = md.render(text);
     expect(result).toMatchInlineSnapshot(`
-      <h1>
-        References
-      </h1>
-      <p>
-        <img src="https://octodex.github.com/images/stormtroopocat.jpg"
-             alt="Stormtroopocat"
-             title="The Stormtroopocat"
-             id="the-stormtroopocat"
-        >
-      </p>
-      <p>
-        <a href="#the-stormtroopocat">
-          Figure 1
-        </a>
-        shows an example.
-      </p>
+      <h1>References</h1>
+      <p><img src="https://octodex.github.com/images/stormtroopocat.jpg" alt="Stormtroopocat" title="The Stormtroopocat" id="the-stormtroopocat" /></p>
+      <p><a href="#the-stormtroopocat">Figure 1</a> shows an example.</p>
     `);
   });
 });
