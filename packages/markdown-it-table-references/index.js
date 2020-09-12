2d59cea6bc2a1a7df2247dd8207cf5d5997f8a4c
Alija Sabic <sabic.alija@gmail.com>
2020-09-12 14:56:27 +0200

    markdown-it-table-references: remove render functions and replace with token stream injection for better plugin ecosystem support

--- packages/markdown-it-table-references/index.js
+++ packages/markdown-it-table-references/index.js
@@ -1,19 +1,17 @@
+const Token = require("markdown-it/lib/token");
+const { isSpace } = require("markdown-it/lib/common/utils");
+
 const table_references = (md, opts) => {
   opts = loadOptions(opts);
 
   md.core.ruler.push("table_reference", table_reference_rule(opts));
   md.core.ruler.push("table_reference_list", table_reference_list_rule(opts));
-
-  md.renderer.rules.table_reference_list_item = table_reference_list_item_renderer(opts);
-  md.renderer.rules.table_wrapper_open = table_wrapper_open_renderer(opts);
-  md.renderer.rules.table_wrapper_close = table_wrapper_close_renderer(opts);
 };
 
 function table_reference_rule(opts) {
   const table_reference = (state /* , silent */) => {
     let tableOpenPos = 0;
-    const tokens = state.tokens;
-    const { isSpace } = state.md.utils;
+    let tokens = state.tokens;
 
     for (let i = 0; i < tokens.length; i++) {
       const { type, content } = tokens[i];
@@ -27,7 +25,7 @@ function table_reference_rule(opts) {
             let m = rProp.exec(match);
             if (!m) continue;
             const [figure, id, caption] = m;
-            addTable(state, opts, id, caption);
+            add_table(state, opts, id, caption);
 
             if (opts.anchor.enable || opts.label.enable) {
               const start = content.substring(0, match.index);
@@ -52,7 +50,7 @@ function table_reference_rule(opts) {
             let m = rProp.exec(match);
             if (!m) continue;
             const [table, id, caption] = m;
-            addTable(state, opts, id, caption);
+            add_table(state, opts, id, caption);
 
             if (opts.anchor.enable || opts.label.enable) {
               const start = content.substring(0, match.index);
@@ -92,7 +90,7 @@ function table_reference_rule(opts) {
           break;
         }
 
-        let id, caption;
+        let id, caption, token;
         const tableCaption = tokens[i + 2];
         if (tableCaption.content.split("#").length === 2) {
           [id, caption] = tableCaption.content.substring(1).split("#");
@@ -101,63 +99,70 @@ function table_reference_rule(opts) {
           id = slugify(caption);
         }
 
-        const tableOpen = tokens[tableOpenPos];
-        tableOpen.attrSet("id", id);
-        tableOpen.type = "table_wrapper_open";
-        tableOpen.meta = { id, caption };
+        add_table(state, opts, id, caption);
+        const index = state.env[opts.ns].refs[id].index;
 
-        const tableClose = tokens[i];
-        tableClose.type = "table_wrapper_close";
-        tableClose.meta = { origin: id, caption };
+        if (opts.wrap) {
+          const pre = tokens.slice(0, tableOpenPos);
+          token = new Token("figure_open", "figure", 1);
+          token.attrSet("id", id);
+          token.block = true;
+          token.meta = { id, caption };
+          pre.push(token);
+
+          i = i + pre.length - tableOpenPos;
+          tokens = [...pre, ...tokens.slice(tableOpenPos)];
+          state.tokens = tokens;
+        } else {
+          token = tokens[tableOpenPos];
+          token.attrSet("id", id);
+
+          const pre = tokens.slice(0, tableOpenPos + 1);
+          token = new Token("table_caption_open", "caption", 1);
+          pre.push(token);
+
+          generate_caption(pre, id, index, caption, opts);
+
+          token = new Token("table_caption_close", "caption", -1);
+          pre.push(token);
+
+          i = i + pre.length - tableOpenPos - 1;
+          tokens = [...pre, ...tokens.slice(tableOpenPos + 1)];
+          state.tokens = tokens;
+        }
+
+        if (opts.wrap) {
+          const pre = tokens.slice(0, i + 1);
+          let children = [];
+
+          token = new Token("figure_caption_open", "figcaption", 1);
+          token.block = opts.anchor.enable || opts.label.enable;
+          children.push(token);
+
+          generate_caption(children, id, index, caption, opts);
+
+          token = new Token("figure_caption_close", "figcaption", -1);
+          token.block = true;
+          children.push(token);
+
+          token = new Token("inline", "", 0);
+          token.children = children;
+          pre.push(token);
+
+          token = new Token("figure_close", "figure", -1);
+          token.block = true;
+          pre.push(token);
+
+          const post = tokens.slice(i + 1);
+          i = pre.length - 1;
+          tokens = [...pre, ...post];
+          state.tokens = tokens;
+        }
+
+        token = tokens[i];
+        token.meta = { origin: id, caption };
 
         tokens.splice(i + 1, 3);
-
-        addTable(state, opts, id, caption);
-
-        if (!opts.wrap) {
-          const tableCaptionOpen = new state.Token("table_caption_open", "caption", 1);
-
-          const tableCaptionContentChildren = [];
-          if (opts.anchor.enable) {
-            const tableCaptionAnchorOpen = new state.Token("link_open", "a", 1);
-            tableCaptionAnchorOpen.attrSet("href", `#${id}`);
-            tableCaptionAnchorOpen.attrSet("class", opts.anchor.class);
-            tableCaptionContentChildren.push(tableCaptionAnchorOpen);
-
-            const tableCaptionAnchorText = new state.Token("text", "", 0);
-            tableCaptionAnchorText.content = opts.anchor.content;
-            tableCaptionContentChildren.push(tableCaptionAnchorText);
-
-            const tableCaptionAnchorClose = new state.Token("link_close", "a", -1);
-            tableCaptionContentChildren.push(tableCaptionAnchorClose);
-          }
-          if (opts.label.enable) {
-            const tableCaptionLabelOpen = new state.Token("link_open", "a", 1);
-            tableCaptionLabelOpen.attrSet("href", `#${id}`);
-            tableCaptionLabelOpen.attrSet("class", opts.label.class);
-            tableCaptionContentChildren.push(tableCaptionLabelOpen);
-
-            const entry = state.env[opts.ns].refs[id];
-            const tableCaptionLabelContent = new state.Token("text", "", 0);
-            tableCaptionLabelContent.content = opts.label.text.replace(opts.label.placeholder, entry.index);
-            tableCaptionContentChildren.push(tableCaptionLabelContent);
-
-            const tableCaptionLabelClose = new state.Token("link_close", "a", -1);
-            tableCaptionContentChildren.push(tableCaptionLabelClose);
-          }
-          const sep = opts.anchor.enable || opts.label.enable ? ": " : "";
-          const tableCaptionText = new state.Token("text", "", 0);
-          tableCaptionText.content = sep + caption;
-          tableCaptionContentChildren.push(tableCaptionText);
-
-          const tableCaptionContent = new state.Token("inline", "", 0);
-          tableCaptionContent.content = sep + caption;
-          tableCaptionContent.children = [...tableCaptionContentChildren];
-
-          const tableCaptionClose = new state.Token("table_caption_close", "caption", -1);
-
-          tokens.splice(tableOpenPos + 1, 0, tableCaptionOpen, tableCaptionContent, tableCaptionClose);
-        }
       }
     }
   };
@@ -170,94 +175,97 @@ function table_reference_list_rule(opts) {
     const tokens = state.tokens;
     let token, tokenChild;
 
-    if (opts.list.enable && opts.list.title !== "") {
-      token = new state.Token("heading_open", "h2", 1);
+    if (opts.list.title !== "") {
+      token = new Token("heading_open", "h2", 1);
       token.attrSet("id", "list-of-tables");
       token.attrSet("class", opts.list.class);
       token.markup = "##";
       token.block = true;
       tokens.push(token);
 
-      token = new state.Token("inline", "", 0);
-      tokenChild = new state.Token("text", "", 0);
+      token = new Token("inline", "", 0);
+      tokenChild = new Token("text", "", 0);
       tokenChild.content = opts.list.title;
       token.children = [tokenChild];
       tokens.push(token);
 
-      token = new state.Token("heading_close", "h2", -1);
+      token = new Token("heading_close", "h2", -1);
       tokens.push(token);
     }
 
-    token = new state.Token("table_reference_list_open", opts.list.tag, 1);
+    token = new Token("table_reference_list_open", opts.list.tag, 1);
     token.attrSet("class", opts.list.class);
     token.block = true;
     tokens.push(token);
 
     for (const id in state.env[opts.ns].refs) {
       const entry = state.env[opts.ns].refs[id];
-      token = new state.Token("table_reference_list_item", opts.list.item.tag, 0);
+      token = new Token("table_reference_list_item_open", opts.list.item.tag, 1);
       token.attrSet("class", opts.list.item.class);
       token.meta = { ...entry };
       tokens.push(token);
+
+      const children = [];
+      if (opts.list.item.label) {
+        children.push(
+          ...generate_link(
+            opts.list.item.href ? `#${id}` : "",
+            opts.label.class,
+            opts.label.text.replace(opts.label.placeholder, entry.index)
+          )
+        );
+      }
+      if (opts.list.item.title) {
+        token = new state.Token("text", "", 0);
+        token.content =
+          opts.list.item.label && opts.list.item.title && entry.caption ? `: ${entry.caption}` : entry.caption;
+        children.push(token);
+      }
+
+      token = new state.Token("inline", "", 0);
+      token.children = children;
+      tokens.push(token);
+
+      token = new state.Token("table_reference_list_item_close", opts.list.item.tag, -1);
+      tokens.push(token);
     }
 
-    token = new state.Token("table_reference_list_close", opts.list.tag, -1);
+    token = new Token("table_reference_list_close", opts.list.tag, -1);
     token.block = true;
     tokens.push(token);
   };
   return table_reference_list;
 }
 
-function table_reference_list_item_renderer(opts) {
-  return (tokens, idx, options, env /* , self */) => {
-    const token = tokens[idx];
-    const id = token.meta.id;
-    const index = env[opts.ns].refs[id].index;
-    const label = render_item_label(id, index, opts);
-    const sep = opts.list.item.title && label && token.meta.caption ? ": " : "";
-    const title = opts.list.item.title ? token.meta.caption : "";
-    return `  <${token.tag} class="${token.attrGet("class")}">${label}${sep}${title}</${token.tag}>\n`;
-  };
+function generate_caption(tokens, id, index, caption, opts) {
+  if (opts.anchor.enable) {
+    tokens.push(...generate_link(`#${id}`, opts.anchor.class, opts.anchor.content));
+  }
+  if (opts.label.enable) {
+    tokens.push(...generate_link(`#${id}`, opts.label.class, opts.label.text.replace(opts.label.placeholder, index)));
+  }
+  if (caption) {
+    token = new Token("text", "", 0);
+    token.content = opts.label.enable ? `: ${caption}` : caption;
+    tokens.push(token);
+  }
 }
 
-function table_wrapper_open_renderer(opts) {
-  return (tokens, idx, options, env /* , self */) => {
-    const token = tokens[idx];
-    const id = token.attrGet("id");
-    return opts.wrap ? `<figure id="${id}">\n<${token.tag}>\n` : `<${token.tag} id="${id}">\n`;
-  };
-}
+function generate_link(href, className, content) {
+  const tokens = [];
+  let token = new Token("link_open", "a", 1);
+  if (href) token.attrSet("href", href);
+  if (className) token.attrSet("class", className);
+  tokens.push(token);
 
-function table_wrapper_close_renderer(opts) {
-  return (tokens, idx, options, env /* , self */) => {
-    const token = tokens[idx];
-    const id = token.meta.origin;
-    const caption = token.meta.caption;
-    const entry = env[opts.ns].refs[id];
-    const anchor = render_anchor(id, opts);
-    const label = render_label(id, entry.index, opts);
-    return opts.wrap
-      ? `</${token.tag}>\n` +
-          `  <figcaption>\n` +
-          `    ${anchor}${label}${label && caption ? ": " : ""}${caption}\n` +
-          `  </figcaption>\n` +
-          `</figure>\n`
-      : `</${token.tag}>\n`;
-  };
-}
+  token = new Token("text", "", 0);
+  token.content = content;
+  tokens.push(token);
 
-function loadOptions(options) {
-  const item = Object.assign(
-    {},
-    table_references.defaults.list.item,
-    options && options.list && options.list.item ? options.list.item : {}
-  );
-  if (options && options.list) options.list.item = item;
-  const list = Object.assign({}, table_references.defaults.list, options && options.list ? options.list : {});
-  const label = Object.assign({}, table_references.defaults.label, options && options.label ? options.label : {});
-  const anchor = Object.assign({}, table_references.defaults.anchor, options && options.anchor ? options.anchor : {});
-  const opts = Object.assign({}, options, { list, label, anchor });
-  return Object.assign({}, table_references.defaults, opts);
+  token = new Token("link_close", "a", -1);
+  tokens.push(token);
+
+  return tokens;
 }
 
 function render_anchor(id, opts) {
@@ -269,14 +277,7 @@ function render_label(id, index, opts) {
   return opts.label.enable ? `<a href="#${id}" class="${opts.label.class}">${label}</a>` : "";
 }
 
-function render_item_label(id, index, opts) {
-  const label = opts.label.text.replace(opts.label.placeholder, index);
-  return opts.list.item.label
-    ? `<a${opts.list.item.href ? ` href="#${id}"` : ""} class="${opts.label.class}">${label}</a>`
-    : "";
-}
-
-function addTable(state, opts, id, caption) {
+function add_table(state, opts, id, caption) {
   if (!state.env[opts.ns]) {
     state.env[opts.ns] = {};
   }
@@ -295,10 +296,43 @@ function slugify(text) {
   return text.replace(/[^\w]/g, "-").replace(/\-+/g, "-").replace(/\-$/, "").toLowerCase();
 }
 
+function loadOptions(options) {
+  return options
+    ? {
+        ns: typeof options.ns === "string" && options.ns ? options.ns : table_references.defaults.ns,
+        wrap: typeof options.wrap === "boolean" ? options.wrap : table_references.defaults.wrap,
+        anchor: Object.assign({}, table_references.defaults.anchor, options.anchor ? options.anchor : {}),
+        label: Object.assign({}, table_references.defaults.label, options.label ? options.label : {}),
+        list: {
+          enable:
+            options.list && typeof options.list.enable === "boolean"
+              ? options.list.enable
+              : table_references.defaults.list.enable,
+          class:
+            options.list && typeof options.list.class === "string"
+              ? options.list.class
+              : table_references.defaults.list.class,
+          title:
+            options.list && typeof options.list.title === "string"
+              ? options.list.title
+              : table_references.defaults.list.title,
+          tag:
+            options.list && typeof options.list.tag === "string"
+              ? options.list.tag
+              : table_references.defaults.list.tag,
+          item: Object.assign(
+            {},
+            table_references.defaults.list.item,
+            options.list && options.list.item ? options.list.item : {}
+          ),
+        },
+      }
+    : table_references.defaults;
+}
+
 table_references.defaults = {
   ns: "tables",
   wrap: true,
-  label: "Table",
   anchor: {
     enable: true,
     content: "§",
